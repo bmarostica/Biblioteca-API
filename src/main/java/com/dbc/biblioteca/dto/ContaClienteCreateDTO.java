aecbb4d2e894985e97e1e905f63cdfef11861edf
Marcelo <marcelo.ungaretti@gmail.com>
2021-11-05 15:19:03 -0300

    Correção anotações - ordem

--- src/main/java/com/dbc/biblioteca/dto/ContaClienteCreateDTO.java
+++ src/main/java/com/dbc/biblioteca/dto/ContaClienteCreateDTO.java
@@ -15,18 +15,18 @@ import javax.validation.constraints.*;
 @NoArgsConstructor
 public class ContaClienteCreateDTO {
 
-    @NotBlank
+    @NotNull
     @ApiModelProperty(value = "Tipo do Cliente -> 0 para comum e 1 para premium")
     @Min(0)
     @Max(1)
     private Integer tipoCliente;
 
     @NotEmpty
-    @NotBlank
+    @NotNull
     @ApiModelProperty(value = "Nome do cliente")
     private String nome;
 
-    @NotBlank
+    @NotNull
     @ApiModelProperty(value = "Telefone do Cliente")
     private String telefone;
 
@@ -35,15 +35,15 @@ public class ContaClienteCreateDTO {
     @ApiModelProperty(value = "Email do Cliente")
     private String email;
 
-    @NotBlank
+    @NotNull
     @ApiModelProperty(value = "Livro do Cliente")
     private LivroEntity livro;
 
-    @NotBlank
+    @NotNull
     @ApiModelProperty(value = "Status do Cliente -> 1 para OK, 2 para CANCELADO e 3 para BLOQUEADO")
     private StatusCliente status;
 
-    @NotBlank
+    @NotNull
     @ApiModelProperty(value = "Quantidade de pontos fidelidade do cliente")
     private Integer pontosFidelidade;
 }
--- src/main/java/com/dbc/biblioteca/dto/ContaClienteDTO.java
+++ src/main/java/com/dbc/biblioteca/dto/ContaClienteDTO.java
@@ -1,5 +1,6 @@
 package com.dbc.biblioteca.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonPropertyOrder(value = { "idCliente", "nome" }, alphabetic = true)
 public class ContaClienteDTO extends ContaClienteCreateDTO{
     private Integer idCliente;
 }
