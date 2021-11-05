44f40ee7c0e4aadf01bb26cc789e5afd6e4396fc
David <davidsallespro@gmail.com>
2021-11-05 17:42:39 -0300

    Api quase pronta, falta apenas os métodos para mudar o status do livro quando for feito um empréstimo e quando o livro for devolvido.

--- src/main/java/com/dbc/biblioteca/repository/EmprestimoRepository.java
+++ src/main/java/com/dbc/biblioteca/repository/EmprestimoRepository.java
@@ -2,11 +2,13 @@ package com.dbc.biblioteca.repository;
 
 import com.dbc.biblioteca.entity.EmprestimoEntity;
 import com.dbc.biblioteca.exceptions.RegraDeNegocioException;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class EmprestimoRepository {
     private static List<EmprestimoEntity> listaEmprestimoEntities = new ArrayList<>();
     private AtomicInteger COUNTER = new AtomicInteger();
