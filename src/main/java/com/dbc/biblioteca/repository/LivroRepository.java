91f19b5b477885a1317b10fcd8372f2d64ea700d
Bianca Marostica <85890342+bmarostica@users.noreply.github.com>
2021-11-05 14:27:20 -0300

    Inserção dos métodos em Livro repository

--- src/main/java/com/dbc/biblioteca/repository/LivroRepository.java
+++ src/main/java/com/dbc/biblioteca/repository/LivroRepository.java
@@ -1,11 +1,13 @@
 package com.dbc.biblioteca.repository;
 
 import com.dbc.biblioteca.entity.LivroEntity;
+import com.dbc.biblioteca.exceptions.RegraDeNegocioException;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class LivroRepository {
@@ -19,22 +21,46 @@ public class LivroRepository {
         livros.add(new LivroEntity(COUNTER.incrementAndGet(), "Bruxa Natural", "Arin Murphy-Hiscock", "Darkside", 256, 1, 0, 0));
     }
 
-    public LivroEntity create(LivroEntity livroEntity){
+    public LivroEntity create(LivroEntity livroEntity) {
         livroEntity.setIdLivro(COUNTER.incrementAndGet());
         livros.add(livroEntity);
         return livroEntity;
     }
 
-    public List<LivroEntity> list(){
+    public List<LivroEntity> list() {
         return livros;
     }
 
-    public List<LivroEntity> listByNome(LivroEntity livroEntity){
+    public List<LivroEntity> listByName(String titulo) {
         return livros.stream()
-                .filter(livro -> livroEntity.)
-
+                .filter(livro -> livro.getTitulo().equalsIgnoreCase(titulo))
+                .collect(Collectors.toList());
     }
 
+    public LivroEntity update(Integer id, LivroEntity livroAtualizar) throws RegraDeNegocioException {
+        LivroEntity livroRecuperado = livros.stream()
+                .filter(livro -> livro.getIdLivro().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RegraDeNegocioException("Livro não localizado"));
+
+        livroRecuperado.setTitulo(livroAtualizar.getTitulo());
+        livroRecuperado.setAutor(livroAtualizar.getAutor());
+        livroRecuperado.setEditora(livroAtualizar.getEditora());
+        livroRecuperado.setNumeroDePaginas(livroAtualizar.getNumeroDePaginas());
+        livroRecuperado.setFormato(livroAtualizar.getFormato());
+        livroRecuperado.setIdioma(livroAtualizar.getIdioma());
+
+        return livroRecuperado;
+    }
+
+    public void delete(Integer id) throws RegraDeNegocioException {
+        LivroEntity livroRecuperado = livros.stream()
+                .filter(livro -> livro.getIdLivro().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RegraDeNegocioException("Livro não localizado"));
+
+        livros.remove(livroRecuperado);
+    }
 
 
 
