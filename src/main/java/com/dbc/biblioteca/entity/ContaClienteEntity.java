57de7cd4c82aff77c1eb325eebc91d03fd6a7bd4
camile <camilelopes.oliveira@gmail.com>
2021-11-12 10:05:42 -0300

    'cliente entity alterado para jpa'

--- src/main/java/com/dbc/biblioteca/entity/ContaClienteEntity.java
+++ src/main/java/com/dbc/biblioteca/entity/ContaClienteEntity.java
@@ -5,16 +5,34 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "cliente")
 public class ContaClienteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
     private Integer idCliente;
+
+    @Column(name = "tipo_cliente")
     private Integer tipoCliente;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "telefone")
     private String telefone;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "status")
     private Integer status;
+
+    @Column(name = "pontos_fidelidade")
     private Integer pontosFidelidade;
 
 }
